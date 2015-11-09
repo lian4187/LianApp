2a35ed1189c73808727c1df728ceeb024cb8d8c9
lian <lian@lian.com>
2015-11-09 21:49:26 +0800

    update

--- Lian/Base/Dao/Joke.php
+++ Lian/Base/Dao/Joke.php
@@ -18,14 +18,14 @@ class Joke
      */
     public static function getOneJoke()
     {
-        $pageNum = mt_rand(1, 100000);
+        $pageNum = mt_rand(1, 10000);
         $url = 'http://apis.baidu.com/hihelpsme/chinajoke/getjokelist?page=' . $pageNum;
         $dataArr = array(
             'sendData' => 'apiKey:' . Configure::API_KEY,
         );
         $apiRes = Curl::send($url, $dataArr);
         $result = json_decode($apiRes, true);
-        $jokeNum = rand(0, 19);
+        $jokeNum = mt_rand(0, 19);
         $oneJokeStr = $result['res_body']['JokeList'][$jokeNum]['JokeContent'];
         return $oneJokeStr;
     }
